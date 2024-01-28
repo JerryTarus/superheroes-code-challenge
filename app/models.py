e48e4b9a4cafc69fb13f5bcfd0b9d5c1b1df5f18
Jerry Tarus <jerrytarus@gmail.com>
2024-01-28 22:23:03 +0300

    Added hero power model

---
+++ app/models.py
@@ -0,0 +1,40 @@
+#### ------ JERRY TARUS ----- ######
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+
+from datetime import datetime 
+
+db = SQLAlchemy()
+
+class Hero(db.Model):
+    __tablename__ = 'hero'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    super_name = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, server_default=db.func.now(), onupdate=datetime.utcnow)
+    powers = db.relationship('Hero_power', back_populates='hero') 
+
+    def __repr__(self):
+        return f'<hero {self.name}>'
+
+
+
+# add any models you may need. 
+
+
+class Hero_power(db.Model):  
+    __tablename__ = 'hero_powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String(255), nullable=False)
+    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'), nullable=False)
+    powers_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False) 
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, server_default=db.func.now(), onupdate=datetime.utcnow)
+    hero = db.relationship('Hero', back_populates='powers') 
+    power = db.relationship('Power', back_populates='heroes')
+
+
