9da384f9e8091ed449963d8fd870100f05793c9f
Jerry Tarus <jerrytarus@gmail.com>
2024-01-28 22:28:28 +0300

    Added model Power

--- app/models.py
+++ app/models.py
@@ -43,3 +43,20 @@ class Hero_power(db.Model):
         if not any(substring in strength for substring in strength_list):
             raise ValueError("Failed simple hero powers validation")
         return strength
+
+
+class Power(db.Model): 
+    __tablename__ = 'powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, server_default=db.func.now(), onupdate=datetime.utcnow)
+    heroes = db.relationship('Hero_power', back_populates='power') 
+
+    @validates('description')
+    def validates_power(self, key, description):
+        if len(description) < 20:
+            raise ValueError("`description` must be present and at least 20 characters long")
+        return description
